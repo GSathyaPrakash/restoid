bfd59c2f05a6b75607a4e0f55a7c640957e9950a
hddq <git@hddq.org>
2026-07-15 23:45:39 +0200

    release: 0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ val hasReleaseSigning = listOf(
     signingKeyPassword
 ).all { !it.isNullOrBlank() }
 
-val baseAppVersionCode = 21
+val baseAppVersionCode = 22
 
 android {
     namespace = "io.github.hddq.restoid"
@@ -55,8 +55,8 @@ android {
         applicationId = "io.github.hddq.restoid"
         minSdk = 33
         targetSdk = 37
-        versionCode = 21
-        versionName = "0.6.2"
+        versionCode = 22
+        versionName = "0.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
